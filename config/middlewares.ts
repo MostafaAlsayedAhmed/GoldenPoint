297cf6eaa6fbdad0e280663753e368170d41d230
MostafaAli <mostly.1st@gmail.com>
2025-11-29 18:55:47 +0200

    update strapi::cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  // 'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://goldenpoint.com', 'http://localhost:3000', 'https://goldenpoint-production.up.railway.app'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
